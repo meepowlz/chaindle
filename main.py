6bf006430c66db492c2a66e26ab58ad2671de724
meepowlz <meepowlz@gmail.com>
2022-04-10 19:30:59 -0400

    i hope this works

---
+++ main.py
@@ -0,0 +1,44 @@
+# Chaindle
+"""
+Todo:
+- Distinguish between number of letters
+    (ex. if there is only one A, guesses with multiple As will only highlight one)
+- Distinguish between guesses prior to correct position
+    (ex. if a user guesses a AGAPE for SHAME, only the second A should highlight
+"""
+import random
+
+word_list = ["apple", "tacos", "words", "games", "water"]
+
+
+def select_word():
+    index = random.randint(0, len(word_list) - 1)
+    print(index)
+    print(word_list[index])
+    return word_list[index]
+
+
+def check_word(user_guess, answer):
+    if user_guess == answer:
+        print("You guessed the word correctly!")
+        return True
+    else:
+        for i, letter in enumerate(user_guess):
+            if letter in answer:
+                if letter == answer[i]:
+                    print(f"{letter} is in the right spot")
+                else:
+                    print(f"{letter} is in the word")
+        return False
+
+
+def main():
+    user_win = False
+    answer = select_word()
+    while not user_win:
+        user_guess = input("Make a guess").lower()
+        user_win = check_word(user_guess, answer)
+
+
+main()
+
